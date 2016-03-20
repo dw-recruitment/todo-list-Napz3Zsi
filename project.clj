1838da22d430cd19e55d482fe40ffe513b834be1
Anonymous Candidate <anon@example.com>
2016-03-20 10:59:13 -0500

    Complete Step 1

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject vote "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ring/ring-core "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [compojure "1.2.0-SNAPSHOT"]
+                 [hiccup "1.0.5"]]
+  :plugins [[lein-ring "0.8.11"]]
+  :ring {:handler vote.core/app})
