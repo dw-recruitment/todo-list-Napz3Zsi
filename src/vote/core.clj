89f26fc05cc9c2283b9587926da165d28d39e49c
Anonymous Candidate <anon@example.com>
2016-03-20 11:02:01 -0500

    Complete Step 2

--- src/vote/core.clj
+++ src/vote/core.clj
@@ -1,1 +1,30 @@
-(ns vote.core)
+(ns vote.core
+(:use ring.adapter.jetty)
+  (:use ring.util.response)
+  (:use compojure.core)
+  (:use hiccup.page hiccup.element)
+  (:require [ring.middleware.resource :as resource]
+            [ring.middleware.file-info :as file-info]
+            [compojure.handler :as handler]
+            [compojure.route :as route]
+            [vote.core :as core]))
+
+(defn index []
+  (html5
+    [:html
+     [:head
+      [:title "Software Developer"]]
+     [:body
+      [:h1 "Web Applicationing Development Using Clojure"]
+      [:h2 "UNDER CONSTRUCTION"]
+      [:img {:src "http://c71123.com/img/jk_keller-under_construction.gif"}]
+      ]]))
+
+(defroutes app-routes
+  (GET "/" [] (index))
+  (route/not-found "Not Found"))
+
+(def app
+  (handler/site app-routes))
+
+(run-jetty #'app {:port 3500})
